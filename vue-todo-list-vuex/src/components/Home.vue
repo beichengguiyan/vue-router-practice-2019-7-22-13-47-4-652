a4ae4ea6c968f133f8c1ee6308e913cd062c5ce6
beichengguiyan <chihaotian@zybank.com.cn>
2019-09-04 23:36:08 +0800

    add css

--- vue-todo-list-vuex/src/components/Home.vue
+++ vue-todo-list-vuex/src/components/Home.vue
@@ -2,11 +2,13 @@
   <div>
     <h1>欢迎{{userName}}</h1>
 
-    <button @click="handleReturnBox">返回</button>
+    <button id="para1" @click="handleReturnBox">返回</button>
+    
+    <p id="para2">用户：{{userName}}</p>
     <br />
-    <router-link :to="'/Home/'+userName+'/TodoList'">TodoList列表</router-link>
+    <router-link :to="'/Home/'+userName+'/TodoList'" id="para3">TodoList列表</router-link>
     <br />
-    <router-link :to="'/Home/'+userName+'/My'">我的</router-link>
+    <router-link :to="'/Home/'+userName+'/My'" id="para4">我的</router-link>
 
     <router-view></router-view>
   </div>
@@ -27,7 +29,33 @@ export default {
 </script>
 
 <style scoped>
+#para1
+{
+  position:fixed;
+  top:30px;
+  left:10px;
+	text-align:center;
+	color:red;
+} 
 
+#para2
+{
+  position:fixed;
+  top:30px;
+  right:10px;
+} 
+
+#para3
+{
+  position:fixed;
+  left:10px;
+} 
+
+#para4
+{
+  position:fixed;
+  left:10px;
+} 
 
 </style>
 
